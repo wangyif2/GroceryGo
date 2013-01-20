dedfef17552f9e33501d2b6a69c4861bc69953da
yancheva <maria.yancheva@gmail.com>
2013-01-20 13:44:31 -0500

    Updated database schema of the test SQLite database for the Crawler.

--- Server/src/com/groceryotg/crawler/crawl.py
+++ Server/src/com/groceryotg/crawler/crawl.py
@@ -29,6 +29,8 @@ flyerURL = {"metro": "http://www.metro.ca/en/on/accessible-flyer.html?method=get
             "sobeys":"http://sob.ca.flyerservices.com/cached_banner_pages/publication.aspx?BannerName=SOB"}
 
 def getFlyer():
+    # Get store URLs from the database Store table
+    
     for url in flyerURL.values():
         if url:
             print("Crawling URL: ", url)
@@ -37,9 +39,34 @@ def getFlyer():
             #print(content)
 
 def createSchema():
-    sql = "create table if not exists Items (id INTEGER PRIMARY KEY, store TEXT, storeLocation TEXT, " + \
-            "rawItem TEXT, category TEXT, price REAL, unit TEXT, dateStart TEXT, dateEnd TEXT)"
+    
+    sql = "CREATE TABLE if not exists Grocery (_id INTEGER PRIMARY KEY, item_id INTEGER, raw_string TEXT, " + \
+            "unit_price REAL, unit_type_id INTEGER, total_price REAL, start_date TEXT, end_date TEXT, " + \
+            "line_number INTEGER, store_id INTEGER, update_date TEXT)"
     cur.execute(sql)
+    sql = "CREATE TABLE if not exists Item (item_id INTEGER PRIMARY KEY, item_name TEXT, subcategory_id INTEGER)"
+    cur.execute(sql)
+    sql = "CREATE TABLE if not exists Store (store_id INTEGER PRIMARY KEY, store_name TEXT, store_address TEXT, store_url TEXT)"
+    cur.execute(sql)
+    sql = "CREATE TABLE if not exists Category (category_id INTEGER PRIMARY KEY, category_name TEXT)"
+    cur.execute(sql)
+    sql = "CREATE TABLE if not exists Subcategory (subcategory_id INTEGER PRIMARY KEY, subcategory_name TEXT, category_id INTEGER)"
+    cur.execute(sql)
+    
+    # Re-create the Store entries
+    sql = "DELETE FROM Store"
+    cur.execute(sql)
+    sql = "INSERT INTO Store (store_name, store_url) VALUES('metro', 'http://www.metro.ca/en/on/accessible-flyer.html?method=getAccessibleFlyer&idFlyer=1576')"
+    cur.execute(sql)
+    sql = "INSERT INTO Store (store_name, store_url) VALUES('loblaws', 'http://eflyerontario.loblaws.ca/cached_banner_pages/publication.aspx?BannerName=LOB')"
+    cur.execute(sql)
+    sql = "INSERT INTO Store (store_name, store_url) VALUES('foodbasics', 'http://www.foodbasics.ca/en/circulaire-accessible.html?method=getAccessibleFlyer&idFlyer=229')"
+    cur.execute(sql)
+    sql = "INSERT INTO Store (store_name, store_url) VALUES('nofrills', 'http://shopnofrills.ca/LCLOnline/flyers_landing_page.jsp?flyerView=accessible&flyerId=28100120')"
+    cur.execute(sql)
+    sql = "INSERT INTO Store (store_name, store_url) VALUES('sobeys', 'http://sob.ca.flyerservices.com/cached_banner_pages/publication.aspx?BannerName=SOB')"
+    cur.execute(sql)
+    
     con.commit()
 
 

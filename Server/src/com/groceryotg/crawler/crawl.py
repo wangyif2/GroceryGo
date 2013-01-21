e381e37b6112fb683bc79677cb35e77defe835be
yancheva <maria.yancheva@gmail.com>
2013-01-21 14:58:56 -0500

    Added .sql dump of category, subcategory, item and nutrient data from Health Canada.

--- Server/src/com/groceryotg/crawler/crawl.py
+++ Server/src/com/groceryotg/crawler/crawl.py
@@ -3,92 +3,68 @@
 # 2. Loblaws
 # 3. Food Basics
 # 4. No Frills
-# 5. Price Chopper - PDF (the images have clear black borders)
-# 6. Sobeys
+# 4. Sobeys
 #
 # Requirements:
-# Python 3.x or higher
+# Python 2.x (the MySQLdb module has not been updated to support Python 3.x yet)
+# To set the python version in Eclipse: Window -> Preferences -> PyDev -> Interpreter-Python
 #
-# Possible categories:
-# - dairy
-# - breads & bakery
-# - produce
-# - meat
-# - seafood
-# - non-edible / household items
-# 
-
-import sqlite3 as lite
-import urllib.request
 
 
-flyerURL = {"metro": "http://www.metro.ca/en/on/accessible-flyer.html?method=getAccessibleFlyer&idFlyer=1576",
-            "loblaws": "http://eflyerontario.loblaws.ca/cached_banner_pages/publication.aspx?BannerName=LOB",
-            "foodbasics":"http://www.foodbasics.ca/en/circulaire-accessible.html?method=getAccessibleFlyer&idFlyer=229",
-            "nofrills":"http://shopnofrills.ca/LCLOnline/flyers_landing_page.jsp?flyerView=accessible&flyerId=28100120",
-            "sobeys":"http://sob.ca.flyerservices.com/cached_banner_pages/publication.aspx?BannerName=SOB"}
+import MySQLdb as mdb
+import sqlalchemy
+from subprocess import Popen, PIPE
+import sys
+import urllib
+import urllib2
+
+
+# Fill in your MySQL user & password
+mysql_user = "USERNAME"
+mysql_password = "PASSWORD"
+mysql_db = "groceryotg"
+mysql_filename = "groceryotg.sql"
+
 
 def getFlyer():
     # Get store URLs from the database Store table
-    
-    for url in flyerURL.values():
-        if url:
-            print("Crawling URL: ", url)
-            f = urllib.request.urlopen(url)
+    cur.execute('SELECT * FROM Store ORDER BY store_name')
+    data = cur.fetchall()
+    for record in data:
+        next_url = record[3]
+        if next_url:
+            print("Crawling URL: ", next_url)
+            f = urllib2.urlopen(next_url)
             content = f.read()
             #print(content)
 
+
 def createSchema():
     
-    sql = "CREATE TABLE if not exists Grocery (_id INTEGER PRIMARY KEY, item_id INTEGER, raw_string TEXT, " + \
-            "unit_price REAL, unit_type_id INTEGER, total_price REAL, start_date TEXT, end_date TEXT, " + \
-            "line_number INTEGER, store_id INTEGER, update_date TEXT)"
-    cur.execute(sql)
-    sql = "CREATE TABLE if not exists Item (item_id INTEGER PRIMARY KEY, item_name TEXT, subcategory_id INTEGER)"
-    cur.execute(sql)
-    sql = "CREATE TABLE if not exists Store (store_id INTEGER PRIMARY KEY, store_name TEXT, store_address TEXT, store_url TEXT)"
-    cur.execute(sql)
-    sql = "CREATE TABLE if not exists Category (category_id INTEGER PRIMARY KEY, category_name TEXT)"
-    cur.execute(sql)
-    sql = "CREATE TABLE if not exists Subcategory (subcategory_id INTEGER PRIMARY KEY, subcategory_name TEXT, category_id INTEGER)"
-    cur.execute(sql)
+    # Execute the .sql file to create the database
+    process = Popen('mysql %s -u%s -p%s' % (mysql_db, mysql_user, mysql_password), \
+                    stdout=PIPE, stdin=PIPE, shell=True)
+    output = process.communicate('source ' + mysql_filename)[0]
     
-    # Re-create the Store entries
-    sql = "DELETE FROM Store"
-    cur.execute(sql)
-    sql = "INSERT INTO Store (store_name, store_url) VALUES('metro', 'http://www.metro.ca/en/on/accessible-flyer.html?method=getAccessibleFlyer&idFlyer=1576')"
-    cur.execute(sql)
-    sql = "INSERT INTO Store (store_name, store_url) VALUES('loblaws', 'http://eflyerontario.loblaws.ca/cached_banner_pages/publication.aspx?BannerName=LOB')"
-    cur.execute(sql)
-    sql = "INSERT INTO Store (store_name, store_url) VALUES('foodbasics', 'http://www.foodbasics.ca/en/circulaire-accessible.html?method=getAccessibleFlyer&idFlyer=229')"
-    cur.execute(sql)
-    sql = "INSERT INTO Store (store_name, store_url) VALUES('nofrills', 'http://shopnofrills.ca/LCLOnline/flyers_landing_page.jsp?flyerView=accessible&flyerId=28100120')"
-    cur.execute(sql)
-    sql = "INSERT INTO Store (store_name, store_url) VALUES('sobeys', 'http://sob.ca.flyerservices.com/cached_banner_pages/publication.aspx?BannerName=SOB')"
-    cur.execute(sql)
     
-    con.commit()
-
-
 con = None
-con = lite.connect('test.db')
+# Uncomment below to create database:
+#createSchema()
 
-with con:   
-    # Testing connection with database:------
+try:
+    con = mdb.connect('localhost', mysql_user, mysql_password, mysql_db)
     cur = con.cursor()
-    cur.execute('SELECT SQLITE_VERSION()')
-    data = cur.fetchone()
-    print("SQLite version: ", data)
-    createSchema()
-    # ---------------------------------------
+    print("connected to database")
     
-    # Crawl all the flyers and parse into (item, price) pairs
-    getFlyer()
+    # SQLAlchemy is a Python ORM
+    print("SQLAlchemy version: ", sqlalchemy.__version__)
     
-    # Do categorization of each item line based on trained classifier
-    # ...
+    #getFlyer()
     
-    # Write to database
-    # ...
-    
-    print("done")
+except mdb.Error, e:
+    print("error: %s" % e)
+    sys.exit(1)
+finally:
+    if con:
+        con.close()
+        print("closed connection to database")

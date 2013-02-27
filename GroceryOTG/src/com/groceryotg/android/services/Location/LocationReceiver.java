9bc4297bb753319df12ddbeebfb905d0fd2949b1
John <john@john-linux.(none)>
2013-02-26 22:20:19 -0500

    adding new file for location receiver

---
+++ GroceryOTG/src/com/groceryotg/android/services/Location/LocationReceiver.java
@@ -0,0 +1,30 @@
+package com.groceryotg.android.services.Location;
+
+import android.location.*;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.app.Notification;
+import android.app.NotificationManager;
+
+/**
+ * User: John
+ * Date: 2013-02-12
+ */
+public class LocationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Location loc = (Location)intent.getExtras().get(LocationMonitor.EXTRA_LOCATION);
+        String msg;
+        
+        if (loc == null) {
+        	msg = intent.getStringExtra(LocationMonitor.EXTRA_ERROR);
+        } else {
+        	msg = loc.toString();
+        }
+        
+        if (msg == null) {
+        	msg = "Invalid location received...";
+        }
+    }
+}

77fa4cd09aafb63fac472f884191ea4dcbb0db7c
robert <robert.yifan.wang@gmail.com>
2013-03-03 17:00:05 -0500

    Added empty sliding menu to grocery overview activity

--- GroceryOTG/src/com/groceryotg/android/GroceryOverView.java
+++ GroceryOTG/src/com/groceryotg/android/GroceryOverView.java
@@ -25,6 +25,7 @@ import com.groceryotg.android.database.contentprovider.GroceryotgProvider;
 import com.groceryotg.android.services.NetworkHandler;
 import com.groceryotg.android.services.ServerURL;
 import com.groceryotg.android.utils.RefreshAnimation;
+import com.slidingmenu.lib.SlidingMenu;
 
 /**
  * User: robert
@@ -41,6 +42,17 @@ public class GroceryOverView extends SherlockListActivity implements LoaderManag
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.grocery_list);
+
+        // configure the SlidingMenu
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu_frame);
 
         Bundle extras = getIntent().getExtras();
 

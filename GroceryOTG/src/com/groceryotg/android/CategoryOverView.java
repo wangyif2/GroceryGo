c97b732def49f41ae6409c0b5c3b28a273d839d3
yancheva <maria.yancheva@gmail.com>
2013-03-06 00:57:17 -0500

    Added icons

--- GroceryOTG/src/com/groceryotg/android/CategoryOverView.java
+++ GroceryOTG/src/com/groceryotg/android/CategoryOverView.java
@@ -1,5 +1,7 @@
 package com.groceryotg.android;
 
+import java.util.Locale;
+
 import android.app.AlarmManager;
 import android.app.LoaderManager;
 import android.app.PendingIntent;
@@ -7,6 +9,7 @@ import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.res.TypedArray;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.location.LocationManager;
@@ -123,7 +126,7 @@ public class CategoryOverView extends SherlockActivity implements LoaderManager.
 		// Set adapter for the grid view
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(adapter);
-
+        
         gridview.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Intent intent = new Intent(CategoryOverView.this, GroceryOverView.class);
@@ -233,19 +236,24 @@ public class CategoryOverView extends SherlockActivity implements LoaderManager.
         private Context mContext;
         private int mLayout;
         private int[] gridColours;
-
+        private TypedArray gridIcons;
+        
         @SuppressWarnings("deprecation")
         public CategoryGridCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
             super(context, layout, c, from, to);
             this.mContext = context;
             this.mLayout = layout;
 
+            // An array of colours for solid background fill
             String[] allColours = mContext.getResources().getStringArray(R.array.colours);
             gridColours = new int[allColours.length];
-
             for (int i = 0; i < allColours.length; i++) {
                 gridColours[i] = Color.parseColor(allColours[i]);
             }
+            
+            // An array of icons (to use instead of colours, when available)
+            gridIcons = mContext.getResources().obtainTypedArray(R.array.icons_arr);
+            
         }
 
         @Override
@@ -258,7 +266,7 @@ public class CategoryOverView extends SherlockActivity implements LoaderManager.
             // Get the next row from the cursor
             String colName = CategoryTable.COLUMN_CATEGORY_NAME;
             String categoryName = c.getString(c.getColumnIndex(colName));
-
+            
             // Set the name of the next category in the grid view
             TextView name_text = (TextView) v.findViewById(R.id.category_row_label);
             if (name_text != null) {
@@ -273,16 +281,37 @@ public class CategoryOverView extends SherlockActivity implements LoaderManager.
 
             // Get the next row from the cursor
             String colName = CategoryTable.COLUMN_CATEGORY_NAME;
-            String categoryName = c.getString(c.getColumnIndex(colName));
-
+            String categoryName = c.getString(c.getColumnIndex(colName)).toLowerCase(Locale.CANADA);
+            
+            // TODO: Update category names in database
+            if (categoryName.equalsIgnoreCase("Miscellaneous")) {
+            	categoryName = "misc";
+            }
+            else if (categoryName.equalsIgnoreCase("Fruits and Vegetables")) {
+            	categoryName = "fruit & veg";
+            }
+            else if (categoryName.equalsIgnoreCase("Bread and Bakery")) {
+            	categoryName = "bread";
+            }
+            else if (categoryName.equalsIgnoreCase("Beverages")) {
+            	categoryName = "drinks";
+            }
+            
+            
             // Set the name of the next category in the grid view
             TextView name_text = (TextView) v.findViewById(R.id.category_row_label);
             if (name_text != null) {
                 name_text.setText(categoryName);
             }
-
+            
             int position = c.getPosition();
-            v.setBackgroundColor(gridColours[position % gridColours.length]);
+            
+            if (position < gridIcons.length()) {
+            	v.setBackgroundResource(gridIcons.getResourceId(position % gridIcons.length(), 0));
+            }
+            else {
+            	v.setBackgroundColor(gridColours[position % gridColours.length]);
+            }
         }
 
     }

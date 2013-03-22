eeaceb58214022ba01dbaea88f6f408db6f3b682
yancheva <maria.yancheva@gmail.com>
2013-03-21 20:06:04 -0400

    Implemented insert, update and remove for watchlist button in Grocery view

--- GroceryOTG/src/com/groceryotg/android/fragment/GroceryListCursorAdapter.java
+++ GroceryOTG/src/com/groceryotg/android/fragment/GroceryListCursorAdapter.java
@@ -45,13 +45,14 @@ public class GroceryListCursorAdapter extends SimpleCursorAdapter {
             {
             	// the "v" parameter represents the just-clicked button/image
             	ImageView icon = (ImageView) v.findViewById(R.id.grocery_row_inshopcart);
+            	
+            	// These flags represent the state of the shoplist and watchlist icons *before* the user clicked
             	TextView tv_selected_shoplist = (TextView) ((LinearLayout)v.getParent()).getChildAt(0);
             	TextView tv_selected_watchlist = (TextView) ((LinearLayout)v.getParent()).getChildAt(2);
             	int shopListFlagBefore = tv_selected_shoplist.getText().toString().equalsIgnoreCase(((Integer)CartTable.FLAG_TRUE).toString()) ? 1 : 0;
             	int watchListFlagBefore = tv_selected_watchlist.getText().toString().equalsIgnoreCase(((Integer)CartTable.FLAG_TRUE).toString()) ? 1 : 0;
             	
-            	
-            	// Get parent view
+            	// Get the row ID and grocery name from the parent view
             	TableLayout tableParent = (TableLayout) v.getParent().getParent().getParent();
             	TextView tv_id = (TextView)((LinearLayout)((TableRow) tableParent.getChildAt(0)).getChildAt(0)).getChildAt(0);
             	TextView tv_name = (TextView)((LinearLayout)((TableRow) tableParent.getChildAt(0)).getChildAt(0)).getChildAt(1);
@@ -72,7 +73,7 @@ public class GroceryListCursorAdapter extends SimpleCursorAdapter {
             		tv_selected_shoplist.setText(((Integer)CartTable.FLAG_FALSE).toString());
             		displayMessage = context.getResources().getString(R.string.cart_shoplist_removed);
             	}
-            	watchListFlag = (tv_selected_watchlist.getText().toString().equalsIgnoreCase(((Integer)CartTable.FLAG_FALSE).toString()) ? CartTable.FLAG_FALSE : CartTable.FLAG_TRUE);
+            	watchListFlag = watchListFlagBefore;
             	icon.setImageResource(newImage);
             	
             	ContentValues values = new ContentValues();
@@ -95,7 +96,7 @@ public class GroceryListCursorAdapter extends SimpleCursorAdapter {
                 	String[] selectionArgs = { tv_id.getText().toString() };
                 	activity.getContentResolver().update(GroceryotgProvider.CONTENT_URI_CART_ITEM, values, whereClause, selectionArgs);
                 }
-                else if (existsInDatabase==CartTable.FLAG_TRUE && shopListFlag==CartTable.FLAG_FALSE) {
+                else if (existsInDatabase==CartTable.FLAG_TRUE && shopListFlag==CartTable.FLAG_FALSE && watchListFlag==CartTable.FLAG_FALSE) {
                 	String whereClause = CartTable.TABLE_CART + "." + CartTable.COLUMN_CART_GROCERY_ID + "=?";
                 	String[] selectionArgs = { tv_id.getText().toString() };
                 	activity.getContentResolver().delete(GroceryotgProvider.CONTENT_URI_CART_ITEM, whereClause, selectionArgs);
@@ -111,7 +112,67 @@ public class GroceryListCursorAdapter extends SimpleCursorAdapter {
         	@Override
             public void onClick(View v) 
             {
-            	Log.d("CUSTOMADAPTER", "Clicked on watchlist icon");
+        		// the "v" parameter represents the just-clicked button/image
+            	ImageView icon = (ImageView) v.findViewById(R.id.grocery_row_inwatchlist);
+            	
+            	// These flags represent the state of the shoplist and watchlist icons *before* the user clicked
+            	TextView tv_selected_shoplist = (TextView) ((LinearLayout)v.getParent()).getChildAt(0);
+            	TextView tv_selected_watchlist = (TextView) ((LinearLayout)v.getParent()).getChildAt(2);
+            	int shopListFlagBefore = tv_selected_shoplist.getText().toString().equalsIgnoreCase(((Integer)CartTable.FLAG_TRUE).toString()) ? 1 : 0;
+            	int watchListFlagBefore = tv_selected_watchlist.getText().toString().equalsIgnoreCase(((Integer)CartTable.FLAG_TRUE).toString()) ? 1 : 0;
+            	
+            	// Get the row ID and grocery name from the parent view
+            	TableLayout tableParent = (TableLayout) v.getParent().getParent().getParent();
+            	TextView tv_id = (TextView)((LinearLayout)((TableRow) tableParent.getChildAt(0)).getChildAt(0)).getChildAt(0);
+            	TextView tv_name = (TextView)((LinearLayout)((TableRow) tableParent.getChildAt(0)).getChildAt(0)).getChildAt(1);
+            	
+            	// Toggle watchlist flag, and keep shoplist flag the same as currently selected
+            	int newImage, shopListFlag, watchListFlag;
+            	String displayMessage;
+            	
+            	if (watchListFlagBefore == CartTable.FLAG_FALSE) {
+            		newImage = R.drawable.ic_star_highlighted;
+            		watchListFlag = CartTable.FLAG_TRUE;
+            		tv_selected_watchlist.setText(((Integer)CartTable.FLAG_TRUE).toString());
+            		displayMessage = context.getResources().getString(R.string.cart_watchlist_added);
+            	}
+            	else {
+            		newImage = R.drawable.ic_star;
+            		watchListFlag = CartTable.FLAG_FALSE;
+            		tv_selected_watchlist.setText(((Integer)CartTable.FLAG_FALSE).toString());
+            		displayMessage = context.getResources().getString(R.string.cart_watchlist_removed);
+            	}
+            	shopListFlag = shopListFlagBefore;
+            	icon.setImageResource(newImage);
+            	
+            	ContentValues values = new ContentValues();
+                values.put(CartTable.COLUMN_CART_GROCERY_ID, tv_id.getText().toString());
+                values.put(CartTable.COLUMN_CART_GROCERY_NAME, tv_name.getText().toString());
+                values.put(CartTable.COLUMN_CART_FLAG_SHOPLIST, shopListFlag);
+                values.put(CartTable.COLUMN_CART_FLAG_WATCHLIST, watchListFlag);
+                
+                int existsInDatabase = CartTable.FLAG_FALSE;
+                if ( shopListFlagBefore == CartTable.FLAG_TRUE || watchListFlagBefore == CartTable.FLAG_TRUE) {
+                	existsInDatabase = CartTable.FLAG_TRUE;
+                }
+                
+                // Determine whether to insert, update, or delete the CartTable entry
+                if (existsInDatabase==CartTable.FLAG_FALSE && watchListFlag==CartTable.FLAG_TRUE) {
+                	activity.getContentResolver().insert(GroceryotgProvider.CONTENT_URI_CART_ITEM, values);
+                }
+                else if (existsInDatabase==CartTable.FLAG_TRUE && watchListFlag==CartTable.FLAG_TRUE) {
+                	String whereClause = CartTable.TABLE_CART + "." + CartTable.COLUMN_CART_GROCERY_ID + "=?";
+                	String[] selectionArgs = { tv_id.getText().toString() };
+                	activity.getContentResolver().update(GroceryotgProvider.CONTENT_URI_CART_ITEM, values, whereClause, selectionArgs);
+                }
+                else if (existsInDatabase==CartTable.FLAG_TRUE && shopListFlag==CartTable.FLAG_FALSE && watchListFlag==CartTable.FLAG_FALSE) {
+                	String whereClause = CartTable.TABLE_CART + "." + CartTable.COLUMN_CART_GROCERY_ID + "=?";
+                	String[] selectionArgs = { tv_id.getText().toString() };
+                	activity.getContentResolver().delete(GroceryotgProvider.CONTENT_URI_CART_ITEM, whereClause, selectionArgs);
+                }
+                	
+                Toast t = Toast.makeText(activity, displayMessage, Toast.LENGTH_SHORT);
+                t.show();
             }
         });
 

77a3a5506fb4b1e697419ca4c931a002406378f0
robert <robert.yifan.wang@gmail.com>
2013-03-23 00:46:14 -0400

    Set up setting skeleton

---
+++ GroceryOTG/src/com/groceryotg/android/settings/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.groceryotg.android.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * User: robert
+ * Date: 22/03/13
+ */
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ GroceryOTG/src/com/groceryotg/android/settings/SettingsFragment.java
@@ -0,0 +1,16 @@
+package com.groceryotg.android.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * User: robert
+ * Date: 22/03/13
+ */
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}

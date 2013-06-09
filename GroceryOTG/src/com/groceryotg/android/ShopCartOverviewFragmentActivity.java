da180a30b3d263d4a88b08a0c0a0288a5d276a72
adlerjohn <adler.john+github@gmail.com>
2013-06-09 15:17:26 -0400

    Almost done global search support for 2.3 #60

--- GroceryOTG/src/com/groceryotg/android/ShopCartOverviewFragmentActivity.java
+++ GroceryOTG/src/com/groceryotg/android/ShopCartOverviewFragmentActivity.java
@@ -8,7 +8,6 @@ public class ShopCartOverviewFragmentActivity extends SherlockFragmentActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.shopcart_list);
         ShopCartOverviewFragment frag = new ShopCartOverviewFragment();
         frag.setHasOptionsMenu(true);
         getSupportFragmentManager().beginTransaction().replace(android.R.id.content, frag).commit();

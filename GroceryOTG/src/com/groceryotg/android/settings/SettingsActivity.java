415d312766bf3d02795f23fb6e3aac0bb599c5a8
adlerjohn <adler.john+github@gmail.com>
2013-06-09 02:26:03 -0400

    #60
    Fixing MultiListPreference support for 2.3 using wrapper

--- GroceryOTG/src/com/groceryotg/android/settings/SettingsActivity.java
+++ GroceryOTG/src/com/groceryotg/android/settings/SettingsActivity.java
@@ -1,18 +1,14 @@
 package com.groceryotg.android.settings;
 
-import android.app.Activity;
+import com.groceryotg.android.R;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-/**
- * User: robert
- * Date: 22/03/13
- */
-public class SettingsActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
-                new SettingsFragment()).commit();
+public class SettingsActivity extends PreferenceActivity {
+    @SuppressWarnings("deprecation")
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	addPreferencesFromResource(R.xml.preferences);
     }
 }

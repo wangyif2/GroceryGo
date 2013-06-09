20f5de1a3ce45b8c1f91d88f12ad1029048f6520
adlerjohn <adler.john+github@gmail.com>
2013-06-09 18:13:04 -0400

    Adding home (up) button for Settings screen. #60

--- GroceryOTG/src/com/groceryotg/android/settings/SettingsActivity.java
+++ GroceryOTG/src/com/groceryotg/android/settings/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.groceryotg.android.settings;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.groceryotg.android.GroceryFragmentActivity;
 import com.groceryotg.android.R;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
@@ -11,5 +15,29 @@ public class SettingsActivity extends SherlockPreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	addPreferencesFromResource(R.xml.preferences);
+    	
+    	configActionBar();
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+            	// This is called when the Home (Up) button is pressed
+                // in the Action Bar. This handles Android < 4.1.
+            	
+            	// Specify the parent activity
+            	Intent parentActivityIntent = new Intent(this, GroceryFragmentActivity.class);
+            	parentActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
+            								Intent.FLAG_ACTIVITY_NEW_TASK);
+            	startActivity(parentActivityIntent);
+            	this.finish();
+            	return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    private void configActionBar() {
+    	getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }

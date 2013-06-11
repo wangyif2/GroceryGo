88284a540f332d1aee6367ee0937fee09909803d
adlerjohn <adler.john+github@gmail.com>
2013-06-11 16:36:48 -0400

    Improving about dialog. #72

--- GroceryOTG/src/com/groceryotg/android/fragment/AboutDialogFragment.java
+++ GroceryOTG/src/com/groceryotg/android/fragment/AboutDialogFragment.java
@@ -1,14 +1,20 @@
 package com.groceryotg.android.fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.groceryotg.android.R;
 
 public class AboutDialogFragment extends SherlockDialogFragment {
+	public AboutDialogFragment() {
+		
+	}
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,7 +23,9 @@ public class AboutDialogFragment extends SherlockDialogFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.about, container, false);
-		//View textView = view.findViewById(R.id.about_text);
+		
+		Dialog d = getDialog();
+		d.setTitle(R.string.about_title);
 		
 		return view;
 	}

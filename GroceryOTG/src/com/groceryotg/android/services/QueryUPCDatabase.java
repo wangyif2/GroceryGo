4cb0283472605a7565395636c3870fbe1bccd6a4
adlerjohn <adler.john+github@gmail.com>
2013-06-20 16:30:47 -0400

    Fixing some bugs with barcode scanner.

--- GroceryOTG/src/com/groceryotg/android/services/QueryUPCDatabase.java
+++ GroceryOTG/src/com/groceryotg/android/services/QueryUPCDatabase.java
@@ -6,9 +6,16 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.StringWriter;
 import java.net.URL;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -28,7 +35,10 @@ public class QueryUPCDatabase extends AsyncTask<String, Void, String> {
 	@Override
 	protected String doInBackground(String... params) {
 		try {
-			URL databaseURL = new URL("http://api.upcdatabase.org/xml/" + params[0] + "/" + params[1]);
+			URL databaseURL = new URL("http://upcdatabase.org/api/xml/" + params[0] + "/" + params[1]);
+			
+			Log.i("GroceryOTG", "http://upcdatabase.org/api/xml/" + params[0] + "/" + params[1]);
+			
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			
@@ -42,13 +52,17 @@ public class QueryUPCDatabase extends AsyncTask<String, Void, String> {
 			NodeList itemNameList = ((Element) node).getElementsByTagName("itemname");
 			if (itemNameList.getLength() == 0)
 				return null;
+			String itemName = nodeToString(itemNameList.item(0));
 			
-			String itemName = itemNameList.item(0).toString();
+			NodeList descriptionList = ((Element) node).getElementsByTagName("description");
+			if (descriptionList.getLength() > 0)
+				itemName += nodeToString(descriptionList.item(0));
 			
 			// TODO: Only get the first item for now, maybe add them as a list later
 			return itemName;
 			
 		} catch (Exception e) {
+			e.printStackTrace();
 			Log.e("GroceryOTG", "Could not construct upc database url. Exception " + e);
 		}
 		
@@ -65,5 +79,19 @@ public class QueryUPCDatabase extends AsyncTask<String, Void, String> {
 	
 	private void makeToast(String text) {
 		Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
+	}
+	
+	private String nodeToString(Node node) {
+		StringWriter sw = new StringWriter();
+		try {
+			Transformer t = TransformerFactory.newInstance().newTransformer();
+			t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			t.setOutputProperty(OutputKeys.INDENT, "yes");
+			t.setOutputProperty(OutputKeys.METHOD, "text");
+			t.transform(new DOMSource(node), new StreamResult(sw));
+		} catch (TransformerException te) {
+			System.out.println("nodeToString Transformer Exception");
+		}
+		return sw.toString();
 	}
 }

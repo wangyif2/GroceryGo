6bbd81e0cfec30afc5a5fb03fe96a504b9bc0ba6
robert <robert.yifan.wang@gmail.com>
2013-06-21 22:21:19 -0600

    setup permissions for GCM on the client side

---
+++ GroceryOTG/src/com/groceryotg/android/gcm/GroceryGCMBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package com.groceryotg.android.gcm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * User: robert
+ * Date: 21/06/13
+ */
+public class GroceryGCMBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}

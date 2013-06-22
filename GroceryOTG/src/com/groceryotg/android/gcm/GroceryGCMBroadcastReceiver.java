fb941a11ecb652432dd1475a35965886e02b8666
robert <robert.yifan.wang@gmail.com>
2013-06-22 00:30:14 -0600

    setup skeleton for google app engine server

--- GroceryOTG/src/com/groceryotg/android/gcm/GroceryGCMBroadcastReceiver.java
+++ GroceryOTG/src/com/groceryotg/android/gcm/GroceryGCMBroadcastReceiver.java
@@ -3,13 +3,18 @@ package com.groceryotg.android.gcm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import com.groceryotg.android.GroceryApplication;
 
 /**
  * User: robert
  * Date: 21/06/13
  */
 public class GroceryGCMBroadcastReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Log.i(GroceryApplication.TAG, intent.getExtras().toString());
     }
 }
---
+++ Server-AppEngine-GCM/web/index.jsp
@@ -0,0 +1,13 @@
+<%--
+  User: robert
+  Date: 22/06/13
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>

21b1180f146d8c7e6b63b38ab281006965358565
adlerjohn <adler.john+github@gmail.com>
2013-06-23 00:36:13 -0400

    Refactoring shopcart add view. Now everything is done from a single fragment. Voice and barcode additions are activated through the action bar.

--- GroceryOTG/src/com/groceryotg/android/ShopCartAddFragmentActivity.java
+++ GroceryOTG/src/com/groceryotg/android/ShopCartAddFragmentActivity.java
@@ -1,14 +1,18 @@
 package com.groceryotg.android;
 
-import android.content.Context;
+import java.util.ArrayList;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.speech.RecognizerIntent;
 import android.support.v4.widget.DrawerLayout;
-import android.view.inputmethod.InputMethodManager;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.ListView;
-import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
@@ -16,19 +20,16 @@ import com.actionbarsherlock.view.MenuItem;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import com.groceryotg.android.fragment.ShopCartAddTabCodeFragment;
-import com.groceryotg.android.fragment.ShopCartAddTabTextFragment;
-import com.groceryotg.android.fragment.ShopCartAddTabVoiceFragment;
+import com.groceryotg.android.services.QueryUPCDatabase;
 import com.groceryotg.android.utils.GroceryOTGUtils;
 
-public class ShopCartAddFragmentActivity extends SherlockFragmentActivity implements ActionBar.TabListener {
+public class ShopCartAddFragmentActivity extends SherlockFragmentActivity {
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1000;
+	
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
-	private ActionBar mActionBar;
 	
-	private final String SAVE_INSTANCE_TAB_INDEX = "save_instance_tab_index";
 	private final String SAVE_INSTANCE_CODE_TEXT = "save_instance_code_text";
-	private int mIndex;
 	
 	private String mCodeText;
 	
@@ -41,21 +42,10 @@ public class ShopCartAddFragmentActivity extends SherlockFragmentActivity implem
 		GroceryOTGUtils.NavigationDrawerBundle drawerBundle = GroceryOTGUtils.configNavigationDrawer(this, false, R.string.title_cart_detail);
 		this.mDrawerLayout = drawerBundle.getDrawerLayout();
 		this.mDrawerList = drawerBundle.getDrawerList();
-		
-		// set up action bar tabs
-		mActionBar = getSupportActionBar();
-		mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
-		mActionBar.addTab(mActionBar.newTab().setText(R.string.title_cart_add_tab_text).setTabListener(this));
-		mActionBar.addTab(mActionBar.newTab().setText(R.string.title_cart_add_tab_voice).setTabListener(this));
-		mActionBar.addTab(mActionBar.newTab().setText(R.string.title_cart_add_tab_code).setTabListener(this));
-		
-		mIndex = 0;
 	}
 	
 	@Override
 	public void onSaveInstanceState(Bundle savedInstanceState) {
-		savedInstanceState.putInt(SAVE_INSTANCE_TAB_INDEX, mIndex);
 		savedInstanceState.putString(SAVE_INSTANCE_CODE_TEXT, mCodeText);
 		
 		super.onSaveInstanceState(savedInstanceState);
@@ -65,15 +55,12 @@ public class ShopCartAddFragmentActivity extends SherlockFragmentActivity implem
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
 	    super.onRestoreInstanceState(savedInstanceState);
 	    
-	    mIndex = savedInstanceState.getInt(SAVE_INSTANCE_TAB_INDEX);
 	    mCodeText = savedInstanceState.getString(SAVE_INSTANCE_CODE_TEXT);
 	}
 	
 	@Override
 	public void onResume() {
 		super.onResume();
-		
-		mActionBar.setSelectedNavigationItem(mIndex);
 	}
 	
 	@Override
@@ -94,69 +81,36 @@ public class ShopCartAddFragmentActivity extends SherlockFragmentActivity implem
 		
 		return super.onPrepareOptionsMenu(menu);
 	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-			case android.R.id.home:
-				if (mDrawerLayout.isDrawerOpen(mDrawerList))
-					mDrawerLayout.closeDrawer(mDrawerList);
-				else {
-					// Specify the parent activity
-					Intent parentActivityIntent = new Intent(this, ShopCartOverviewFragmentActivity.class);
-					parentActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
-												Intent.FLAG_ACTIVITY_NEW_TASK);
-					startActivity(parentActivityIntent);
-					this.finish();
-				}
-				return true;
+		case R.id.cart_add_by_voice:
+			Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+			intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+			intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.cart_add_voice_prompt));
+			intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+			intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+			startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+			break;
+		case R.id.cart_add_by_barcode:
+			IntentIntegrator integrator = new IntentIntegrator(this);
+			integrator.initiateScan();
+			break;
+		case android.R.id.home:
+			if (mDrawerLayout.isDrawerOpen(mDrawerList))
+				mDrawerLayout.closeDrawer(mDrawerList);
+			else {
+				// Specify the parent activity
+				Intent parentActivityIntent = new Intent(this, ShopCartOverviewFragmentActivity.class);
+				parentActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
+											Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(parentActivityIntent);
+				this.finish();
+			}
+			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	@Override
-	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		String title = (String) tab.getText();
-		
-		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.hideSoftInputFromWindow(findViewById(R.id.content).getWindowToken(), 0);
-		
-		if (title == getString(R.string.title_cart_add_tab_text)) {
-			mIndex = 0;
-			ft.replace(R.id.content, new ShopCartAddTabTextFragment());
-		} else if (title == getString(R.string.title_cart_add_tab_voice)) {
-			ft.replace(R.id.content, new ShopCartAddTabVoiceFragment());
-			mIndex = 1;
-		} else if (title == getString(R.string.title_cart_add_tab_code)) {
-			ShopCartAddTabCodeFragment frag = new ShopCartAddTabCodeFragment();
-			
-			Bundle args = new Bundle();
-			args.putString(ShopCartAddTabCodeFragment.SHOP_CART_ADD_CODE_TEXT_ARG, mCodeText);
-			frag.setArguments(args);
-			
-			ft.replace(R.id.content, frag);
-			mIndex = 2;
-		}
-	}
-
-	@Override
-	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-		if ((String) tab.getText() == getString(R.string.title_cart_add_tab_code)) {
-			mCodeText = null;
-		}
-	}
-
-	@Override
-	public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		if (tab.getText() == getString(R.string.title_cart_add_tab_code)) {
-			ShopCartAddTabCodeFragment frag = new ShopCartAddTabCodeFragment();
-			
-			Bundle args = new Bundle();
-			args.putString(ShopCartAddTabCodeFragment.SHOP_CART_ADD_CODE_TEXT_ARG, mCodeText);
-			frag.setArguments(args);
-			
-			ft.replace(R.id.content, frag);
-		}
 	}
 	
 	// Need to add this here since the ZXing library creates a new activity from activity, not fragment
@@ -165,6 +119,29 @@ public class ShopCartAddFragmentActivity extends SherlockFragmentActivity implem
 		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 		if (scanResult != null) {
 			mCodeText = scanResult.getContents();
+			setCode(mCodeText);
+		}
+		else if (requestCode == VOICE_RECOGNITION_REQUEST_CODE) {
+			if (resultCode == Activity.RESULT_OK) {
+			
+			ArrayList<String> textMatchList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+			 
+			if (!textMatchList.isEmpty()) {
+				String text = textMatchList.get(0);
+				TextView textView = (TextView) findViewById(R.id.cart_grocery_edit_name);
+				textView.setText(text);
+			}
+			} else if (resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+				makeToast("Audio error");
+			} else if (resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+				makeToast("Client error");
+			} else if (resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+				makeToast("Network error");
+			} else if (resultCode == RecognizerIntent.RESULT_NO_MATCH){
+				makeToast("Please repeat the item");
+			} else if (resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+				makeToast("Server error");
+			}
 		}
 		
 		super.onActivityResult(requestCode, resultCode, data);
@@ -172,6 +149,24 @@ public class ShopCartAddFragmentActivity extends SherlockFragmentActivity implem
 	
 	public void clearCodeText() {
 		mCodeText = null;
+	}
+	
+	private void makeToast(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+	
+	private void setCode(String code) {
+		if (code == null)
+			return;
+		
+		EditText nameView = (EditText) findViewById(R.id.cart_grocery_edit_name);
+		String oldHint = (String) nameView.getHint();
+		nameView.setHint("Searching for item " + code + " in the database...");
+		
+		QueryUPCDatabase q = new QueryUPCDatabase(this, nameView);
+		q.execute(getString(R.string.upcdatabase_key), code);
+		
+		nameView.setHint(oldHint);
 	}
 }
 
--- GroceryOTG/src/com/groceryotg/android/fragment/ShopCartAddFragment.java
+++ GroceryOTG/src/com/groceryotg/android/fragment/ShopCartAddFragment.java
@@ -5,29 +5,27 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.actionbarsherlock.app.SherlockFragment;
-import com.google.zxing.integration.android.IntentIntegrator;
 import com.groceryotg.android.R;
-import com.groceryotg.android.ShopCartAddFragmentActivity;
 import com.groceryotg.android.database.CartTable;
 import com.groceryotg.android.database.contentprovider.GroceryotgProvider;
-import com.groceryotg.android.services.QueryUPCDatabase;
 import com.groceryotg.android.utils.GroceryOTGUtils;
 
-public class ShopCartAddTabCodeFragment extends SherlockFragment {
-	public static final String SHOP_CART_ADD_CODE_TEXT_ARG = "add_code_text";
+public class ShopCartAddFragment extends SherlockFragment {
 	private Context mContext;
 	
-	private TextView mText;
-	private String mCodeText;
-	
+	private EditText mEditText;
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
@@ -37,28 +35,24 @@ public class ShopCartAddTabCodeFragment extends SherlockFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		Bundle args = this.getArguments();
-		if (args != null) {
-			mCodeText = args.getString(SHOP_CART_ADD_CODE_TEXT_ARG);
-		}
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.shopcart_add_tab_code, container, false);
+		View v = inflater.inflate(R.layout.shopcart_add_fragment, container, false);
 		
-		mText = (TextView) v.findViewById(R.id.code_text);
-
-		ImageButton micButton = (ImageButton) v.findViewById(R.id.barcode_button);
-		micButton.setOnClickListener(new View.OnClickListener() {
+		mEditText = (EditText) v.findViewById(R.id.cart_grocery_edit_name);
+		mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 			@Override
-			public void onClick(View v) {
-				IntentIntegrator integrator = new IntentIntegrator((Activity) mContext);
-				integrator.initiateScan();
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_GO) {
+					addItem();
+					return true;
+				}
+				return false;
 			}
 		});
-		
+
 		Button confirmButton = (Button) v.findViewById(R.id.positive_button);
 		confirmButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -66,6 +60,7 @@ public class ShopCartAddTabCodeFragment extends SherlockFragment {
 				addItem();
 			}
 		});
+		
 		Button clearButton = (Button) v.findViewById(R.id.negative_button);
 		clearButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -76,39 +71,22 @@ public class ShopCartAddTabCodeFragment extends SherlockFragment {
 		
 		return v;
 	}
-	
-	@Override
-	public void onViewCreated (View view, Bundle savedInstanceState) {
-		setCode(mCodeText);
-	}
-	
-	private void setCode(String code) {
-		if (code == null)
-			return;
-		
-		TextView textView = (TextView) ((Activity) mContext).findViewById(R.id.code_text);
-		String oldHint = (String) textView.getHint();
-		textView.setHint("Searching for item " + code + " in the database...");
-		
-		QueryUPCDatabase q = new QueryUPCDatabase(mContext, mText);
-		q.execute(getString(R.string.upcdatabase_key), code);
-		
-		textView.setHint(oldHint);
-	}
-	
+
 	private void makeToast(String text) {
-		Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
+		Toast.makeText(mContext, text, Toast.LENGTH_LONG).show();
 	}
 	
 	private void clearFocus() {
+		// close the soft keyboard
+		InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
 		// clear the next in the edit box
-		mText.setText("");
-		// clear the saved variable from the activity
-		((ShopCartAddFragmentActivity) mContext).clearCodeText();
+		mEditText.setText("");
+		mEditText.clearFocus();
 	}
 	
 	private void addItem() {
-		String name = mText.getText().toString();
+		String name = mEditText.getText().toString();
 		
 		clearFocus();
 		
@@ -121,6 +99,7 @@ public class ShopCartAddTabCodeFragment extends SherlockFragment {
 
 		ContentValues values = new ContentValues();
 		values.put(CartTable.COLUMN_CART_GROCERY_NAME, name);
+		values.putNull(CartTable.COLUMN_CART_GROCERY_ID);
 		values.put(CartTable.COLUMN_CART_FLAG_SHOPLIST, CartTable.FLAG_TRUE);
 		values.put(CartTable.COLUMN_CART_FLAG_WATCHLIST, CartTable.FLAG_FALSE);
 

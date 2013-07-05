70b25c875bdf583d09e3f396b7283a3e2548c674
robert <robert.yifan.wang@gmail.com>
2013-07-05 14:03:18 -0600

    added back renamed Android/GAE module

---
+++ GroceryGo-Android/src/com/grocerygo/android/database/objects/StoreParent.java
@@ -0,0 +1,31 @@
+package com.grocerygo.android.database.objects;
+
+public class StoreParent {
+	private Integer storeParentId;
+
+	private String storeParentName;
+
+	public StoreParent(String storeParentName) {
+		this.storeParentName = storeParentName;
+	}
+
+	public StoreParent() {
+	}
+
+	public int getStoreParentId() {
+		return storeParentId;
+	}
+
+	public void setStoreParentId(int storeParentId) {
+		this.storeParentId = storeParentId;
+	}
+
+	public String getName() {
+		return storeParentName;
+	}
+
+	public void setName(String storeParentName) {
+		this.storeParentName = storeParentName;
+	}
+
+}
---
+++ GroceryGo-Android/src/com/grocerygo/android/fragment/GlobalSearchFragment.java
@@ -0,0 +1,42 @@
+package com.grocerygo.android.fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.app.SherlockListFragment;
+import com.grocerygo.android.R;
+
+public class GlobalSearchFragment extends SherlockListFragment {
+	private Context mContext;
+	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		this.mContext = activity;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.search_fragment_list, container, false);
+		return v;
+	}
+
+	public void setQuery(String query) {
+		TextView searchTitle = (TextView) ((Activity) mContext).findViewById(R.id.search_title);
+		searchTitle.setText("\"" + query + "\"");
+		
+		GroceryListFragment frag = (GroceryListFragment) ((SherlockFragmentActivity) mContext).getSupportFragmentManager().findFragmentById(R.id.search_fragment_list_fragment);
+		frag.loadDataWithQuery(false, query);
+	}
+}

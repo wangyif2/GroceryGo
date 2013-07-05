84dec031fe6b4a4f4ac7b5ec7999c38bbbd93481
robert <robert.yifan.wang@gmail.com>
2013-07-05 15:11:15 -0600

    trakcing bootstrap in a seperate git now

--- GroceryGo-Bootstrap/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GroceryGoBootstrap::Application.config.secret_token = '567ee88c1ace03944a171b808b2b750ae3e274de9fb19b0951d07b06eabd991241ff5df3fb753fdcd3fba9b894aa571ff344c19804ee9e037e8b76b17d347394'

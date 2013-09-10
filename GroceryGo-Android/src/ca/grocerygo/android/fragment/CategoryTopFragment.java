c2d3b046ee9b0370a58d2b72f1c77f066e819d5c
wangyif2 <robert.yifan.wang@gmail.com>
2013-09-09 23:31:58 -0700

    prompt user to enable wifi

--- GroceryGo-Android/src/ca/grocerygo/android/fragment/CategoryTopFragment.java
+++ GroceryGo-Android/src/ca/grocerygo/android/fragment/CategoryTopFragment.java
@@ -2,8 +2,10 @@ package ca.grocerygo.android.fragment;
 
 import android.app.Activity;
 import android.content.Context;
+import android.database.ContentObserver;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -13,78 +15,101 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.SimpleCursorAdapter;
+import ca.grocerygo.android.GroceryApplication;
 import ca.grocerygo.android.R;
 import ca.grocerygo.android.database.CategoryTable;
 import ca.grocerygo.android.database.contentprovider.GroceryotgProvider;
 import ca.grocerygo.android.utils.GroceryGoUtils;
 import com.actionbarsherlock.app.SherlockFragment;
+import android.util.Log;
 
 public class CategoryTopFragment extends SherlockFragment implements LoaderManager.LoaderCallbacks<Cursor> {
-	private Context mContext;
-	private GridView gridview;
-	private SimpleCursorAdapter mAdapter;
+    private Context mContext;
+    private GridView gridview;
+    private SimpleCursorAdapter mAdapter;
 
-	private final int INDEX_LOADER_CAT = 0;
-	
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-		this.mContext = activity;
-	}
+    public static final int INDEX_LOADER_CAT = 0;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.mContext = activity;
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.category_fragment_list, container, false);
-		gridview = (GridView) v.findViewById(R.id.gridview);
-		gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-				GroceryGoUtils.launchGroceryPagerActivity(mContext, position);
-			}
-		});
-		gridview.setEmptyView(v.findViewById(R.id.empty_category_list));
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		return v;
-	}
+        MyContentObserver myContentObserver = new MyContentObserver(new Handler());
+        getActivity().getContentResolver().registerContentObserver(GroceryotgProvider.CONTENT_URI_CAT, true, myContentObserver);
+    }
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		fillData();
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.category_fragment_list, container, false);
+        gridview = (GridView) v.findViewById(R.id.gridview);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                GroceryGoUtils.launchGroceryPagerActivity(mContext, position);
+            }
+        });
+        gridview.setEmptyView(v.findViewById(R.id.empty_category_list));
 
-	private void fillData() {
-		String[] from = new String[]{CategoryTable.COLUMN_CATEGORY_NAME};
-		int[] to = new int[]{R.id.category_row_label};
+        return v;
+    }
 
-		getLoaderManager().initLoader(INDEX_LOADER_CAT, null, this);
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        fillData();
+    }
 
-		mAdapter = new CategoryTopCursorAdapter(getActivity(), R.layout.category_fragment_list_row, null, from, to);
+    private void fillData() {
+        String[] from = new String[]{CategoryTable.COLUMN_CATEGORY_NAME};
+        int[] to = new int[]{R.id.category_row_label};
 
-		gridview.setAdapter(mAdapter);
+        getLoaderManager().initLoader(INDEX_LOADER_CAT, null, this);
 
-	}
+        mAdapter = new CategoryTopCursorAdapter(getActivity(), R.layout.category_fragment_list_row, null, from, to);
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
-		String[] projection = {CategoryTable.COLUMN_ID, CategoryTable.COLUMN_CATEGORY_NAME};
-		return new CursorLoader(getActivity(), GroceryotgProvider.CONTENT_URI_CAT, projection, null, null, null);
-	}
+        gridview.setAdapter(mAdapter);
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-		SimpleCursorAdapter sa = mAdapter;
-		if (sa != null)
-			sa.changeCursor(cursor);
-	}
+    }
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> cursorLoader) {
-		SimpleCursorAdapter sa = mAdapter;
-		sa.changeCursor(null);
-	}
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
+        String[] projection = {CategoryTable.COLUMN_ID, CategoryTable.COLUMN_CATEGORY_NAME};
+        return new CursorLoader(getActivity(), GroceryotgProvider.CONTENT_URI_CAT, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        SimpleCursorAdapter sa = mAdapter;
+
+        if (sa != null)
+            sa.changeCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        SimpleCursorAdapter sa = mAdapter;
+        sa.changeCursor(null);
+    }
+
+    class MyContentObserver extends ContentObserver {
+        public MyContentObserver(Handler h) {
+            super(h);
+        }
+
+        @Override
+        public boolean deliverSelfNotifications() {
+            return true;
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            Log.i(GroceryApplication.TAG, "S:DLKJA:DKFJA:SLKJF:ALGKJ:LDSAKJF");
+        }
+    }
 }
